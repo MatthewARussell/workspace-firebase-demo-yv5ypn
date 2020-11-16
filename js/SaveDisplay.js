868b5b08a1a721ead3daf151d2b927fafae98a30
MatthewARussell <matthewrussell904@hotmail.com>
2020-11-16 12:57:21 -0500

    1 file modified

--- js/SaveDisplay.js
+++ js/SaveDisplay.js
@@ -5,10 +5,16 @@ var firebaseConfig = {};
 firebase.initializeApp(firebaseConfig);
 
 // save the data
-$(".sampleSurvey input[type='submit']").click(function() {
-
-
-
-  
+$(".sampleSurvey input[type='submit']").click(function(e) {
+  e.preventDefault();
+  //get the value of th eform using SerializeArray method
+  var inputdata = $("form").serializeArray();
+  var surveyList = {};
+  for (var i = 0; i < inputdata.length; i++) {
+    var name = inputdata[i]["name"];
+    var value = inputdata[i]["value"];
+    console.log(name + " " + value);
+    surveyList[name] = value;
+  }
 });
 // update the result in table
